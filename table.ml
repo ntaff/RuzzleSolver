a6e88cda96a56f39e803ad14c13ceda9dff994f9
AdrienFAC <36764462+AdrienFAC@users.noreply.github.com>
2018-12-01 17:22:43 +0100

    Add files via upload

---
+++ table.ml
@@ -0,0 +1,29 @@
+
+(* SOURCE : https://caml.inria.fr/pub/docs/manual-caml-light/node15.8.html *)
+
+#open "hashtbl";;
+
+
+(* recher à l'élément "elem" de la table et renvoit toutes ses valeurs sous forme de liste *)
+let getFromTable = fun (table, elem) -> try
+						find_all table elem
+					with Not_found -> [];;
+
+
+(* Affiche le contenu d'une table /!\ A utiliser avec do_table *)
+let display = fun s v -> print_string("Key: " ^ s ^ " \tValue: " ^ v ^ "\n");;
+
+
+(*************************************************************)
+
+let table = new 1;; (* La veleur après le new n'a pas d'omportance *)
+
+add table "key1" "val1";;
+add table "key1" "val2";;
+add table "key2" "val3";;
+add table "key2" "val4";;
+add table "key1" "val5";;
+
+do_table display table;;
+
+getFromTable(table, "key1");;
