09331ace9dfc2a3bc626e0a0297cf78eacfad72e
AdrienFAC <36764462+AdrienFAC@users.noreply.github.com>
2018-12-04 00:52:01 +0100

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unicodedata
+
+def correct(s):
+    if '-' in s:
+        return False
+    if ' ' in s:
+        return False
+    if '\'' in s:
+        return False
+    if len(s) > 17:
+       return False
+    return True
+
+
+def parseFile():
+   file = open('dictionnaire_min.txt', 'r')
+   content = file.readlines()
+   file.close()
+   lst = sorted(set([elem[:-1] for elem in content if correct(elem)]))
+
+   file = open('dico.txt', 'w', encoding = 'utf-8')
+   for line in lst:
+        file.write(line)
+        file.write('\n')
+   file.close()
+
+parseFile()
+   
