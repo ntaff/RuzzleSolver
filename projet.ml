f81fdbf0ff82e950a5c8f2ddbbedb72d983a3be9
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-12-03 12:14:10 +0100

    Add comments and types of functions

--- projet.ml
+++ projet.ml
@@ -2,44 +2,54 @@
 
 (********************** CHAINE **********************)
 
-let sousChaine = fun (s, n, m) -> if m < n
-									then ""
-								else sub_string s (n - 1) (m - n + 1);;
+
+let sousChaine = fun (s, n, m)
+(* sousChaine : string * int * int -> string = <fun> *)
+-> if m < n then "" else sub_string s (n - 1) (m - n + 1);;
 
 let tetec = fun 
+(* tetec : string -> char = <fun> *)
 "" -> failwith "tetec: chaine vide"
 | s -> nth_char s 0;;
 
-let tetes = fun s -> string_of_char(tetec(s));;
+let tetes = fun s
+(* tetes : string -> string = <fun> *)
+-> string_of_char(tetec(s));;
+
 
 let reste = fun 
+(* reste : string -> string = <fun> *)
 "" -> failwith "La chaine est vide"
 | s -> sousChaine(s, 2, string_length s);;
 
 
 (****************** LECTURE FICHIER ******************)
 
-(* Lit un fichier ligne par ligne à partir de son fd et le renvois sous forme de le liste*)
-(* in_channel -> string list *)
 let rec readFileByLines fd =
+(* readFileByLines : in_channel -> string list = <fun> *)
+(* Lit un fichier ligne par ligne à partir de son fd et le renvois sous forme de le liste*)
 	try
 		let l = input_line fd in
 		l :: readFileByLines(fd)
 	with _ -> [];;
 
 
-(****************************************************)
+(********************* FONCTIONS DE L'ALGORITHME DE RECHERCHE *******************************)
 
 
-(* Remplace la lettre à l'index "i" par un espace*)
-(* string * int -> string *)
 let rec removeLetter = fun
+(* removeLetter : string * int -> string = <fun> *)
+(* Remplace la lettre à l'index "i" par un espace*)
 (s, 0) -> " " ^ reste(s)
 | (s, i) -> tetes(s) ^ removeLetter(reste(s), i - 1);;
 
 
 
 let spaceNext = fun
+(* spaceNext : int * int -> bool = <fun> *)
+(* spaceNext(avant, fin) ---> bool si le déplacement est possible
+   si la case fin est adjacente à la case avant
+   La fonction mem vérifie la condition si last est contenu dans la liste suivante*)
 (_, -1) -> true
 | (0, last) -> mem last [0;4;5]
 | (1, last) ->  mem last [0;2;4;5;6]
@@ -61,6 +71,9 @@ let spaceNext = fun
 
 
 let resetIndex = fun
+(* resetIndex : int -> int = <fun> *)
+(* Fonction d'optimisation permettant de commencer la recherche à la première case possible, évite de
+   re parcourir tout le tableau *)
 0 -> 0
 | i when (i mod 4 >= 1) & ((i - (i mod 4)) / 4 >= 1) -> i - 5
 | i when (i / 4) = 0 -> i - 1
@@ -68,6 +81,8 @@ let resetIndex = fun
 
 
 let rec recherche = fun
+(* recherche : string * string * int * int -> bool = <fun> *)
+(* Fonction de recherche appelant les fonctions annexes et renvois si un mot est constructible à partir du tableau de jeu  *)
 (_, "", _, _) -> true
 | (_, _, 16, _) -> false
 | (p, word, indice, last) -> if p.[indice] = tetec(word) & spaceNext(indice, last)
@@ -78,11 +93,15 @@ let rec recherche = fun
 
 
 let rec start = fun
+(* start : string * string list -> string list = <fun> *)
+(* Renvois une liste de tous les mots possible avec un tableau de jeu donné *)
 (p, x :: l) -> if recherche(p, x, 0, -1)
 					then x :: start(p, l)
 				else start(p, l)
 | _ -> [];;
 
+
+(*********************** JEUX DE TESTS *********************************)
 
 let file = "C:\Users\adrie\Desktop\Projet\dico.txt";;
 
@@ -94,6 +113,7 @@ close_in fd;;
 
 let resultats = start("audnsbceaeblifil", fileContent);;
 
+(* Exemple avec le mot abeille *)
 (*
 trace "spaceNext";;
 trace "suite";;
