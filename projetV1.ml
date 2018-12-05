d059f94bf5835d812a7d6da303d3db12e853d251
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-12-05 09:55:05 +0100

    Refactor

--- projetV1.ml
+++ projetV1.ml
@@ -1,33 +1,37 @@
+(****************************************************)
+(***************PROJET RUZZLE SOLVER*****************)
+(************NICOLAS TAFFOUREAU (@ntaff)*************)
+(************ADRIEN TREILHOU (@AdrienFAC)************)
+(*******************VERSION 1************************)
+(****************************************************)
 
 
 (********************** CHAINE **********************)
 
-
-let sousChaine = fun (s, n, m)
 (* sousChaine : string * int * int -> string = <fun> *)
+let sousChaine = fun (s, n, m)
 -> if m < n then "" else sub_string s (n - 1) (m - n + 1);;
 
-let tetec = fun 
 (* tetec : string -> char = <fun> *)
+let tetec = fun 
 "" -> failwith "tetec: chaine vide"
 | s -> nth_char s 0;;
 
-let tetes = fun s
 (* tetes : string -> string = <fun> *)
+let tetes = fun s
 -> string_of_char(tetec(s));;
 
-
-let reste = fun 
 (* reste : string -> string = <fun> *)
+let reste = fun 
 "" -> failwith "La chaine est vide"
 | s -> sousChaine(s, 2, string_length s);;
 
 
 (****************** LECTURE FICHIER ******************)
 
-let rec readFileByLines fd =
-(* readFileByLines : in_channel -> string list = <fun> *)
 (* Lit un fichier ligne par ligne à partir de son fd et le renvois sous forme de le liste*)
+(* readFileByLines : in_channel -> string list = <fun> *)
+let rec readFileByLines fd =
 	try
 		let l = input_line fd in
 		l :: readFileByLines(fd)
@@ -37,19 +41,19 @@ let rec readFileByLines fd =
 (********************* FONCTIONS DE L'ALGORITHME DE RECHERCHE *******************************)
 
 
-let rec removeLetter = fun
-(* removeLetter : string * int -> string = <fun> *)
 (* Remplace la lettre à l'index "i" par un espace*)
+(* removeLetter : string * int -> string = <fun> *)
+let rec removeLetter = fun
 (s, 0) -> " " ^ reste(s)
 | (s, i) -> tetes(s) ^ removeLetter(reste(s), i - 1);;
 
 
 
-let spaceNext = fun
-(* spaceNext : int * int -> bool = <fun> *)
 (* spaceNext(avant, fin) ---> bool si le déplacement est possible
    si la case fin est adjacente à la case avant
    La fonction mem vérifie la condition si last est contenu dans la liste suivante*)
+(* spaceNext : int * int -> bool = <fun> *)
+let spaceNext = fun
 (_, -1) -> true
 | (0, last) -> mem last [0;4;5]
 | (1, last) ->  mem last [0;2;4;5;6]
@@ -70,19 +74,20 @@ let spaceNext = fun
 | (_, _) -> false;;
 
 
-let resetIndex = fun
-(* resetIndex : int -> int = <fun> *)
+
 (* Fonction d'optimisation permettant de commencer la recherche à la première case possible, évite de
    re parcourir tout le tableau *)
+(* resetIndex : int -> int = <fun> *)
+let resetIndex = fun
 0 -> 0
 | i when (i mod 4 >= 1) & ((i - (i mod 4)) / 4 >= 1) -> i - 5
 | i when (i / 4) = 0 -> i - 1
 | i when (i mod 4) = 0 -> i - 4;;
 
 
-let rec recherche = fun
-(* recherche : string * string * int * int -> bool = <fun> *)
 (* Fonction de recherche appelant les fonctions annexes et renvois si un mot est constructible à partir du tableau de jeu  *)
+(* recherche : string * string * int * int -> bool = <fun> *)
+let rec recherche = fun
 (_, "", _, _) -> true
 | (_, _, 16, _) -> false
 | (p, word, indice, last) -> if p.[indice] = tetec(word) & spaceNext(indice, last)
@@ -92,45 +97,40 @@ let rec recherche = fun
 							else recherche(p, word, indice + 1, last);;
 
 
-let rec start = fun
-(* start : string * string list -> string list = <fun> *)
 (* Renvois une liste de tous les mots possible avec un tableau de jeu donné *)
+(* start : string * string list -> string list = <fun> *)
+let rec start = fun
 (p, x :: l) -> if recherche(p, x, 0, -1)
 					then x :: start(p, l)
 				else start(p, l)
 | _ -> [];;
 
 
-(*********************** JEUX DE TESTS *********************************)
+(*********************** JEU DE TESTS *********************************)
 
+(* Chemin d'accès absolu vers le dictionnaire *)
+let file = "C:\Users\Taffoureau\Documents\dico.txt";;  
+
+(* On ouvre le fichier dans un fd *)
+let fd = open_in(file);; 
+
+(* On tri le dictionnaire par taille de mots puis par ordre alphabétique à l'aide d'un tri Fusion *)
+let fileContent = readFileByLines(fd);; 
+
+(* On ferme le fd *)
+close_in fd;;
+
+(* On définie un tableau de jeu comme étant une chaîne de caractères *)
+let tableaudejeu = "ditamjnaeazgesif";;
+
+(* On prend l'empreinte de temps courante *)
 let time = Sys__time();;
 
-let file = "C:\Users\Taffoureau\Documents\dico.txt";;
+(* On lance la recherche des solution à partir du tableau de jeu donné *)
+let result = start(tableaudejeu, fileContent);; (* 50 mots *)
 
-let fd = open_in(file);;
+(* Combien de temps a pris la recherche *)
+let timefinal = Sys__time() -. time;;
 
-let fileContent = readFileByLines(fd);;
-
-close_in fd;;
-Sys__time() -. time;;
-
-let timebis = Sys__time();;
-let resultats = start("audnsbceaeblifil", fileContent);;
-Sys__time() -. timebis;;
-
-
-(* Temps d'execution
-
-Temps pour la fonction start : 0.375sec
-Temps pour lire le dictionnaire : 0.040sec
-Temps pour la fonction recherche : Trop petit pour être mesuré *)
-
-(* Exemple avec le mot abeille *)
-(*
-trace "spaceNext";;
-trace "suite";;
-trace "depart";;
-trace "start";;
-
-let resultats = start("audnsbceaeblifil", ["abeille"]);;
-*)
+(* On affiche le nombre de résultats et le temps mis pour les trouver *)
+"Pour le tableau de jeu : " ^ tableaudejeu ^ " on trouve " ^ string_of_int(list_length result) ^ " résultats en : " ^ string_of_float(timefinal) ^ " secondes.";;
